6dc525acb6152e3a2a386cf17b23d779bfc9addf
= <=>
2026-02-27 15:31:03 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter, Router } from "react-router-dom";
+
+
+export function App() {
+
+  return (
+      <ThemeProvider theme={defaultTheme}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+      </ThemeProvider>
+  )
+}
+
